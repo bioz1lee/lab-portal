b2bffae7fcfce78da19ae59654a91d606265855b
이지원 <jiwonlee@EZ1-MacBook-Air.local>
2026-03-25 14:04:10 +0900

    Add news: 2025 fall & 2026 spring semester program starts

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -15,8 +15,10 @@ const events = [
 ];
 
 const news = [
+  { date: "March, 2026", text: "Minje Sung, Yujin Kim started their MS-Ph.D integrated program. Welcome!" },
   { date: "December, 2025", text: "Jaehyuk Jeon joined the lab as student intern. Welcome!" },
   { date: "November, 2025", text: "Ina Jeon joined the lab as incoming Master's student. Welcome!" },
+  { date: "September, 2025", text: "Seoyeon Kim started her Ph.D program. Dongwon Yun started his Master's program. Welcome!" },
   { date: "July, 2025", text: "YuJin Kim joined the lab as student intern. Welcome!" },
   { date: "March, 2025", text: "Seoyeon Kim joined the lab as an incoming Ph.D student. Welcome!" },
   { date: "March, 2025", text: "Dongwon Yun, Minje Sung joined the lab as an incoming master's student. Welcome!" },
